e65ec70cf6804e089221e1e3adc0ff73ab2bb3d3
Joé Veber <joeveber@Joes-MacBook-Pro.local>
2022-05-12 11:22:44 +0200

    Netlify deployment test

--- src/pages/Comics/Comics.js
+++ src/pages/Comics/Comics.js
@@ -9,29 +9,35 @@ const Comics = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(
-        `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=6heC5JHaDWef65UA`
-      );
-      setData(response.data);
-      setIsLoading(false);
-      console.log(response.data);
+      try {
+        const response = await axios.get(
+          `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=6heC5JHaDWef65UA`
+        );
+        setData(response.data);
+        setIsLoading(false);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error.message);
+        console.log(error.response);
+      }
     };
     fetchData();
   }, []);
 
-  return (
+  return isLoading === true ? (
+    <h1>En cours de chargement</h1>
+  ) : (
     <div className="carousel">
       <h1>Choissisez vos comics préférés</h1>
 
       {data.results.map((result) => {
         const imagePath = result.thumbnail.path + "/portrait_fantastic.jpg";
-        console.log(imagePath);
         return (
           <Link to={`/comic/${result._id}`} key={result._id}>
             <div className="card">
               <p>{result._id}</p>
               <p>{result.title}</p>
-              <img className="comic-image" src={imagePath} alt="comic-image" />
+              <img className="comic-image" src={imagePath} alt="comic-card" />
             </div>
           </Link>
         );
